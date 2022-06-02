fb906b847f1eb07b0dc148e99a9ce03ff5ce7cfb
klevisdoga <klevisdoga@Mac-mini.local>
2022-06-02 17:23:00 -0400

    UPDATE: upload-page completed... responsive to all display sizes. TO DO: display that the user has published their video and redirect them back to home page

--- src/pages/UploadPage/uploadpage.js
+++ src/pages/UploadPage/uploadpage.js
@@ -12,17 +12,25 @@ export default function UploadPage() {
         <Header />
       </div>
       <div className='upload__body'>
-      <h1 className='upload__title'>Upload Video</h1>
-      <form className='upload__form'>
-        <label htmlFor='uploadImage' className='upload__form-image--text'>VIDEO THUMBNAIL</label>
-        <img src={uploadThumb} alt='thumbnail placeholder' className='upload__form-image'/>
-        <label htmlFor='uploadTitle' className='upload__form-title'>TITLE YOUR VIDEO</label>
-        <input type='text' id='uploadTitle' name='uploadTitle' placeholder='Add a title to your video' className='upload__form-title--box' required/>
-        <label htmlFor='uploadDesc' className='upload__form-description'>ADD A VIDEO DESCRIPTION</label>
-        <textarea id='uploadDesc' name='uploadDesc' placeholder='Add a description to your video' className='upload__form-description--box'/>
-        <Link to='/' className='upload__form-button'>PUBLISH</Link>
-        <Link to='/' className='upload__form-button--cancel'>CANCEL</Link>
-      </form>
+        <h1 className='upload__title'>Upload Video</h1>
+        <form className='upload__form'>
+          <div className='upload__form-container'>
+            <div className='upload__form-container--thumbnail'>
+              <label htmlFor='uploadImage' className='upload__form-image--text'>VIDEO THUMBNAIL</label>
+              <img src={uploadThumb} alt='thumbnail placeholder' className='upload__form-image' />
+            </div>
+            <div className='upload__form-container--info'>
+              <label htmlFor='uploadTitle' className='upload__form-title'>TITLE YOUR VIDEO</label>
+              <input type='text' id='uploadTitle' name='uploadTitle' placeholder='Add a title to your video' className='upload__form-title--box' required />
+              <label htmlFor='uploadDesc' className='upload__form-description'>ADD A VIDEO DESCRIPTION</label>
+              <textarea id='uploadDesc' name='uploadDesc' placeholder='Add a description to your video' className='upload__form-description--box' />
+            </div>
+          </div>
+          <div className='upload__form-buttons'>
+            <Link to='/' className='upload__form-button'>PUBLISH</Link>
+            <Link to='/' className='upload__form-button--cancel'>CANCEL</Link>
+          </div>
+        </form>
       </div>
     </div>
   )
