cb2a9b80c7bbc06c936056b8a31832fde6930b55
klevisdoga <klevisdoga@Mac-mini.local>
2022-06-07 22:10:05 -0400

    UPDATE: all data being recieved from AXIOS call is from local REST API

--- src/Data/videoURL.js
+++ src/Data/videoURL.js
@@ -1,3 +1,3 @@
 export const videos = 'http://localhost:8080/videos'
 export const API_KEY = '?api_key="3bff6375-3185-44ec-be6b-8ed4d824581b"' 
-export const API_LINK = 'https://project-2-api.herokuapp.com/videos/'
+export const API_LINK = 'http://localhost:8080/videos/'
